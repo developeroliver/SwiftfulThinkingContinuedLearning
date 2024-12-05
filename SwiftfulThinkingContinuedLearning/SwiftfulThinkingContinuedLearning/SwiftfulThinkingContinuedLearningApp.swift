b68534b12a84231ecd942e12a4e3f680d97c70fa
codewitholivier as Oliver <92441827+developeroliver@users.noreply.github.com>
2024-12-05 15:39:23 +0100

    init project

---
+++ SwiftfulThinkingContinuedLearning/SwiftfulThinkingContinuedLearning/SwiftfulThinkingContinuedLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulThinkingContinuedLearningApp.swift
+//  SwiftfulThinkingContinuedLearning
+//
+//  Created by olivier geiger on 05/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulThinkingContinuedLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
